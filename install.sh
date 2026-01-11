5562220a8b8b12463c615f8096bcd6cfa4d970d1
Andrew DeVries <adevries@digitalexample.com>
2026-01-11 17:11:26 -0500

    Delete install.sh

--- install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-curl -sL "https://raw.githubusercontent.com/Andrewiski/OpenObserveGoFlowDocker/refs/heads/main/dockerCompose/installOpenObserveGoFlow.sh" -o installOpenObserveGoFlow.sh
-chmod +x installOpenObserveGoFlow.sh
-./installOpenObserveGoFlow.sh -useremail rootuser@example.com -userPassword ComplexPassword123421
